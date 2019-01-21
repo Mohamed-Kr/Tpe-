63a3f837b304cb6840999dea74078773dc332dbe
lefuturiste <mail@matthieubessat.fr>
2019-01-21 14:26:34 +0100

    lel

--- src/views/Intro/1.vue
+++ src/views/Intro/1.vue
@@ -2,14 +2,13 @@
 	<v-container justify-center align-center mt-5>
 		<v-flex pa-3 headline white--text id='introTxt'>
 			A travers ce site, nous avons décider de tenter de répondre à une problématique 
-			importante de notre époque: Est-il possible de produire assez d'éléctricité pour tout le monde 
-			sans que ça n'ait d'impact sur notre planète ?
+			importante pour notre époque et notre futur: 
+			<br>
+			<b>Comment, pourquoi et par quels moyens obtenir un mix énergétique électrique 100% renouvelable en France ?</b>
+			<br>
+			Autrement dit : Est-il possible de produire assez d'éléctricité pour répondre à nos besoin sans que ça n'ait d'impact sur notre planète ?
 			<br/><br/>
-			Nous avons choisis cette problématique car nous pensons qu'elle est trop souvent prise à la légère,
-			particulièrement d'un point de vue politique... 
-			<br/><br/>
-			Mais bien heureusement: ces quelques dérnières années, il y a eu une certaine prise de conscience 
-			sur le sujet
+			Mais avant il convient de préciser les cadres de notre TPE, quel sujet nous traiterons exactement. Notre TPE se concentra principalement sur l'énergie électrique qui est aujourd'hui le principal vecteur énergétique utilisé dans le monde. Il faut savoir que la transition énergétique s'applique dans tout les domaines de l'énergie. En tout cas, sachez que quand vous entenderez "Transition énergétique", comprenez "Transition énergétique électrique". 
 		</v-flex>
 	</v-container>
 </template>
