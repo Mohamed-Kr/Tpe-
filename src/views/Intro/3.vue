7a19688fa049e03527d607e30323d50e5a556459
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-01-21 23:33:09 +0100

    debut intro electricité

--- src/views/Intro/3.vue
+++ src/views/Intro/3.vue
@@ -1,15 +1,69 @@
 <template>
 	<v-container>
-		igyg
+		<v-card class='pt-2'>
+			<v-card class='subheading ma-3 pa-3' flat>
+				<v-card-title class='headline'>Avant de parler de produire de l'éléctricité, soyons sûr de savoir de quoi on parle</v-card-title>
+				<v-card-title class='title mt-5'>Définition</v-card-title>
+				<v-card-text>
+				L'électricité est l'effet du déplacement de particules chargées (les éléctrons) 
+				dans un conducteur sous l'effet d'une difference de potentiel aux extremités de ce conducteur.
+				<br/>
+				L'éléctricité n'est donc pas vraiment de l'énérgie mais plutôt un vecteur énérgétique, un 'moyen de
+				transport' pour l'énérgie.
+				</v-card-text>
+			</v-card>
+			<v-card class='subheading ma-5 pa-2' flat>
+				<v-card-title class='title'>La matière</v-card-title>
+				<v-card-text>
+					<v-layout align-start>
+						<v-flex xs5 mt-5 pt-5>
+					Tout d'abord, rappelons que la matière est constituée d'atomes, formés par 
+					un noyeau, constitué de protons (charges positives) et de neutrons (charges neutre) 
+					et d'un nuage éléctronique, constitué d'éléctrons (charges négatives) 'orbitants' autour du noyeau.<br/>
+					Un atome est éléctriquement neutre et est donc composé d'autant d'éléctrons que de protons.
+						</v-flex>
+						<v-flex xs7 style='height: 500px; overflow: hidden'>
+							<p style='text-align: center' class='title grey--text'>schéma simplifié d'un atome</p>
+							<Elec1/>
+						</v-flex>
+					</v-layout>
+					-Les orbites des éléctrons ne peuvent être situées qu'à certaines distances du noyau, 
+					on parle de couches éléctroniques discrètes.<br/>
+					-Plus la couche est proche du noyau, plus la force qui lie un éléctron au noyau l'est aussi et plus
+					l'énérgie qu'il faut fournir à un éléctron pour le 'séparer' du noyau doit être grande.<br/>
+					-La couche éléctronique situé a la plus grande distance du noyeau (ou couche externe)
+					est donc celle dont les éléctrons sont les plus facile à séparer du noyeau.
+					<v-card class='subheading mt-5 pa-2' flat>
+						<v-card-title class='title'>Les ions</v-card-title>
+						<v-card-text>
+							Il arrive qu'un atome perde ou gagne un ou plusieurs éléctron(s) et devienne donc chargé éléctriquement.
+							<v-layout pa-5>
+								<v-flex xs-6>
+									<p style='text-align: center' class='title grey--text'>schéma simplifié d'un<br/> ion de charge positive</p>
+									<Elec2/>
+								</v-flex>
+								<v-flex xs-6>
+									<p style='text-align: center' class='title grey--text'>schéma simplifié d'un<br/> ion de charge négative</p>
+									<Elec3/>
+								</v-flex>
+							</v-layout>
+						</v-card-text>
+					</v-card>
+				</v-card-text>
+			</v-card>
+		</v-card>
 	</v-container>
 </template>
 
 <script>
+import Elec1 from './schemas/Elec1.vue'
+import Elec2 from './schemas/Elec2.vue'
+import Elec3 from './schemas/Elec3.vue'
 export default {
-
+	components: {
+		Elec1,
+		Elec2,
+		Elec3
+	}
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/views/Intro/schemas/Elec3.vue
@@ -0,0 +1,107 @@
+<template>
+	<svg version="1.1" id="atome" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="-50 71 700 700" style="enable-background:new 0 0 600 600;" xml:space="preserve">
+		<g id="atom">
+			<g id="noyau">
+				<g>
+					<circle class="el30" cx="300" cy="300" r="226.5"/>
+					<circle class="el31" cx="300" cy="300" r="153.5"/>
+					<g>
+						<circle class="el32" cx="300" cy="300" r="84.5"/>
+						<path d="M300,216c46.32,0,84,37.68,84,84s-37.68,84-84,84s-84-37.68-84-84S253.68,216,300,216 M300,215c-46.94,0-85,38.06-85,85
+							s38.06,85,85,85s85-38.06,85-85S346.94,215,300,215L300,215z"/>
+					</g>
+				</g>
+				<g>
+					<g id="proton">
+						<g>
+							<circle class="el33" cx="280" cy="354" r="21.5"/>
+							<polygon class="el34" points="291.5,352.5 282,352.5 282,343 278,343 278,352.5 268.5,352.5 268.5,356.5 278,356.5 278,366 
+								282,366 282,356.5 291.5,356.5 					"/>
+						</g>
+						<g>
+							<circle class="el33" cx="339" cy="330" r="21.5"/>
+							<polygon class="el34" points="350.5,328.5 341,328.5 341,319 337,319 337,328.5 327.5,328.5 327.5,332.5 337,332.5 337,342 
+								341,342 341,332.5 350.5,332.5 					"/>
+						</g>
+						<g>
+							<circle class="el33" cx="252" cy="270" r="21.5"/>
+							<polygon class="el34" points="263.5,268.5 254,268.5 254,259 250,259 250,268.5 240.5,268.5 240.5,272.5 250,272.5 250,282 
+								254,282 254,272.5 263.5,272.5 					"/>
+						</g>
+						<g>
+							<circle class="el33" cx="313" cy="241" r="21.5"/>
+							<polygon class="el34" points="324.5,239.5 315,239.5 315,230 311,230 311,239.5 301.5,239.5 301.5,243.5 311,243.5 311,253 
+								315,253 315,243.5 324.5,243.5 					"/>
+						</g>
+						<g>
+							<circle class="el33" cx="306" cy="298" r="21.5"/>
+							<polygon class="el34" points="317.5,296.5 308,296.5 308,287 304,287 304,296.5 294.5,296.5 294.5,300.5 304,300.5 304,310 
+								308,310 308,300.5 317.5,300.5 					"/>
+						</g>
+					</g>
+					<g id="neutrons">
+						<circle class="el35" cx="246" cy="319" r="21.5"/>
+						<circle class="el35" cx="350" cy="278" r="21.5"/>
+					</g>
+				</g>
+			</g>
+			<g id="elecs3">
+				<g>
+					<circle class="el39" cx="306" cy="146" r="21.5"/>
+					<rect x="294.5" y="144.5" class="el34_" width="23" height="4"/>
+				</g>
+				<g>
+					<circle class="el39" cx="454" cy="300" r="21.5"/>
+					<rect x="442.5" y="298.5" class="el34_" width="23" height="4"/>
+				</g>
+				<g>
+					<circle class="el39" cx="418" cy="397" r="21.5"/>
+					<rect x="406.5" y="395.5" class="el34_" width="23" height="4"/>
+				</g>
+				<g>
+					<circle class="el39" cx="146" cy="287" r="21.5"/>
+					<rect x="134.5" y="285.5" class="el34_" width="23" height="4"/>
+				</g>
+				<g>
+					<circle class="el39" cx="258" cy="447" r="21.5"/>
+					<rect x="246.5" y="445.5" class="el34_" width="23" height="4"/>
+				</g>
+				<g class='new'>
+					<circle class="el39" cx="258" cy="447" r="21.5"/>
+					<rect x="246.5" y="445.5" class="el34_" width="23" height="4"/>
+				</g>
+			</g>
+		</g>
+	</svg>
+</template>
+
+<script>
+import { TimelineMax } from 'gsap'
+export default {
+	mounted() {
+		const elecs = document.getElementById('elecs3')
+		const sign = document.getElementsByClassName('el34_')
+		const tlelec1 = new TimelineMax({onComplete: () => {tlelec1.restart()}})
+		const tlsign = new TimelineMax({onComplete: () => {tlsign.restart()}})
+
+		tlelec1
+		.fromTo(elecs, 3, {rotation: 0, transformOrigin: '50% 50%', ease: Linear.easeNone},
+						  {rotation: 360, transformOrigin: '50% 50%', ease: Linear.easeNone})
+		tlsign
+		.fromTo(sign, 3, {rotation: 360, transformOrigin: '50% 50%', ease: Linear.easeNone},
+						  {rotation: 0, transformOrigin: '50% 50%', ease: Linear.easeNone})
+	}
+}
+</script>
+
+<style type="text/css">
+	.el30{fill:#F2F2F2;stroke:#808080;stroke-miterlimit:10;}
+	.el31{fill:#F2F2F2;stroke:#E6E6E6;stroke-miterlimit:10;}
+	.el32{fill:#999999;}
+	.el33{fill:#E55442;stroke:#808080;stroke-miterlimit:10;}
+	.el34,el34_{fill:#000000;stroke-miterlimit:10;}
+	.el35{fill:#4FB554;stroke:#808080;stroke-miterlimit:10;}
+	.el39{fill:#65BAF5;stroke:#808080;stroke-miterlimit:10;}
+	.new{transform: translate(70px,0px)}
+</style>
