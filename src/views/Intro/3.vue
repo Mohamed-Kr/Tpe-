8ab6c56d345f06e88a0033ef1749f1c89cf0d3fc
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-01-28 19:52:48 +0100

    amélioration de lelectricité

--- src/views/Intro/3.vue
+++ src/views/Intro/3.vue
@@ -1,8 +1,9 @@
 <template>
 	<v-container>
-		<v-card class='pt-2'>
+		<v-card class='pt-2 pb-4'>
 			<v-card class='subheading ma-3 pa-3' flat>
-				<v-card-title class='headline'>Avant de parler de produire de l'éléctricité, soyons sûr de savoir de quoi on parle</v-card-title>
+				<v-card-title class='headline'>Nous parlerons plus tard de production éléctrique, rappellons donc ce qu'est l'éléctricité.</v-card-title>
+				<v-card>
 				<v-card-title class='title mt-5'>Définition</v-card-title>
 				<v-card-text>
 				L'électricité est l'effet du déplacement de particules chargées (les éléctrons) 
@@ -11,8 +12,9 @@
 				L'éléctricité n'est donc pas vraiment de l'énérgie mais plutôt un vecteur énérgétique, un 'moyen de
 				transport' pour l'énérgie.
 				</v-card-text>
+				</v-card>
 			</v-card>
-			<v-card class='subheading ma-5 pa-2' flat>
+			<v-card class='subheading ma-5 pa-2' >
 				<v-card-title class='title'>La matière</v-card-title>
 				<v-card-text>
 					<v-layout align-start>
@@ -34,23 +36,25 @@
 					-La couche éléctronique située à la plus grande distance du noyau (ou couche externe)
 					est donc celle dont les éléctrons sont les plus facile à séparer du noyau, ce qui fait que les matériaux peuvent être 
 					plus ou moin conducteur.
-					<v-card class='subheading mt-5' flat>
-						<v-card-title class='title'>Les ions</v-card-title>
-						<v-card-text>
-							Il arrive qu'un atome perde ou gagne un ou plusieurs éléctron(s), il devient donc un ion éléctriquement chargé.
-							<v-layout pa-5 style='height:450px; overflow: hidden'>
-								<v-flex xs-6>
-									<p style='text-align: center' class='title grey--text'>Schéma simplifié d'un<br/> ion de charge positive</p>
-									<Elec2/>
-								</v-flex>
-								<v-flex xs-6>
-									<p style='text-align: center' class='title grey--text'>Schéma simplifié d'un<br/> ion de charge négative</p>
-									<Elec3/>
-								</v-flex>
-							</v-layout>
-						</v-card-text>
-					</v-card>
-					<v-card class='subheading pa-2' flat>
+				</v-card-text>
+				</v-card>
+				<v-card class='subheading ma-5'>
+					<v-card-title class='title'>Les ions</v-card-title>
+					<v-card-text>
+						Il arrive qu'un atome perde ou gagne un ou plusieurs éléctron(s), il devient alors un ion et est donc éléctriquement chargé.
+						<v-layout pa-5 style='height:450px; overflow: hidden'>
+							<v-flex xs-6>
+								<p style='text-align: center' class='title grey--text'>Schéma simplifié d'un<br/> ion de charge positive</p>
+								<Elec2/>
+							</v-flex>
+							<v-flex xs-6>
+								<p style='text-align: center' class='title grey--text'>Schéma simplifié d'un<br/> ion de charge négative</p>
+								<Elec3/>
+							</v-flex>
+						</v-layout>
+					</v-card-text>
+				</v-card>
+					<v-card class='subheading pa-2 ma-5'>
 						<v-card-title class='title'>Un peu de pratique</v-card-title>
 						Dans ce schéma, il y a deux matériaux de charges éléctriques opposées, appuyez sur le bouton pour les relier avec un matériau conducteur.
 						<Conducteur/>
@@ -59,8 +63,6 @@
 							-Le courant éléctrique est la conséquence du déplacement des éléctrons dans la matière.
 						</v-card-text>
 					</v-card>
-				</v-card-text>
-			</v-card>
 		</v-card>
 	</v-container>
 </template>
