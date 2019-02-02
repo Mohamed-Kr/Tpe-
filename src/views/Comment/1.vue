f28284fea5ca9408d2cb54b6fe578f6f704f357a
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-02-02 21:43:07 +0100

    début partie comment changé fite vait

--- src/views/Comment/1.vue
+++ src/views/Comment/1.vue
@@ -1,17 +1,30 @@
 <template>
-	<v-container>
-		
-Une consommation inconstante:
+<v-container>
+  <v-layout justify-center align-center fill-height>
+    <v-flex xs11 md9 lg10 xl8>
+      <v-card class='subheading mx-3 px-3 transparent' flat>
+        <v-card-title class='headline white--text'>Si on réfléchit bien: sans consomation, pas besoin de production.
+        </v-card-title>
+      </v-card>
+      <v-card class='subheading mx-5 mt-4 pa-3'>
+        <v-card-text>
+          <v-card-title class='title'>Une consommation inconstante</v-card-title>
+          <v-card class='mt-2 px-3' flat>
 La consommation est sensible à la météo et plus particulièrement à la température. Par exemple en hiver, plus il fait froid plus les ménages et les entreprises consomment de l'électricité pour se chauffer. On dit que la consommation est thermosensible.
 De même, la consommation est beaucoup moins élevée la nuit car les industries ne fonctionnent pas et les ménages consomment moins.
-
-Une inconstance problématique:
+          </v-card>
+        </v-card-text>
+      </v-card>
+      <v-card class='subheading ma-5 pa-3'>
+        <v-card-text>
+          <v-card-title class='title'>Une inconstance problématique</v-card-title>
+          <v-card class='mt-2 px-3' flat>
 Cette inconstance est assez problématique au niveau de la production car sur d’aussi grosses échelles, les moyens de stockage électrique actuels sont encore trop peu performants,
 Les moyens de production électrique s’adaptent donc, par exemple, les pales ré-orientables des barrages permettent de concentrer leur production pendant les périodes de forte consommation. Elles sont capables de passer de l'arrêt à leur puissance maximale en quelques minutes (voir partie sur l'hydroélectricité).
 La consommation est pourtant le facteur le plus important car plus nous consommons, plus nous devons produire, nous avons donc le devoir de veiller sur notre consommation.
-
-
-
+          
+		  
+		  
 -la consommation en france reste assez stable, entre 450 TWh et 500 TWh, pourtant la consommation des ménages est en constante augmentation, mais cette augmentation est compensé par la baisse de consommation au niveau des industries due à la mondialisation.
 
 
@@ -23,16 +36,24 @@ ampoules halogènes par des ampoules plus performante (LED, fluorescente)
 l'ADEME: le terme “gisements d'économie d'énergie”
 l'éclairage nocturne inutile
 publicité
-
-	</v-container>
+		  </v-card>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
 export default {
-
+  components: {}
 }
 </script>
 
-<style>
 
-</style>
+
+
+
+
+
+
