06e9e3124a1bc704b388877538a9b62ffc74a66f
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-02-11 18:03:24 +0100

    avanssage hydroelectrique

--- src/views/Pourquoi/moyensProd/Hydro.vue
+++ src/views/Pourquoi/moyensProd/Hydro.vue
@@ -2,49 +2,40 @@
 		<v-card class='pa-4'>
 			<v-card-title class='display-2'>Les barrages hydroéléctriques</v-card-title>
 			<v-card flat class='my-4 px-5 title'>
-				Les énergies fossiles (ou hydrocarbures) sont issues de la transformation de matières 
-				organiques et sont stockées sous terre.
+				Le secteur de l'hydroélectricité en France bénéficie d'un potentiel important grâce à la présence de massifs montagneux : Alpes, Pyrénées, Massif central.
 			</v-card>
 			<v-card class='mt-2 px-3'>
 				<v-card-text>
-					
-Hydraulique
-Cette production représente environ 10% dans le mix national, elle représente 16,6 % de la production mondiale d’électricité, sa part est en très forte augmentation ces dernière années. C’est un moyen de production très appréciés par les pays émergents. En France, c’est un moyen de production très important depuis la fin de la seconde guerre mondiale.
-Part: ≃10%
-Rendement:  ≃82%
-Le parc hydraulique concentre 51 % de la capacité EnR installée en France.
-
-
-Installation à grande échelle
-Simple moyen de production
-Cela a toujours été un moyen de production dans l’histoire de l’énergie
-Gros historique français dans l’hydroélectrique
-Élasticité à la consommation
-Risques et problèmes, catastrophes écologiques etc.
-Le rendement (rapport électricité consommée et électricité produite) est de l'ordre de 82 %.
-
--La production hydroélectrique en France représentait 10,1 % de la production électrique totale en 2017 contre 13,8 % en 2013. Le taux de couverture de la consommation par la production hydraulique atteignait 11,5 % en France en 2016-17.
-- permettent de concentrer leur production pendant les périodes de forte consommation. elles sont capables de passer de l'arrêt à leur puissance maximale en quelques minutes, et donc de faire face aux variations parfois très rapides de la demande ou de l'offre par exemple, lorsqu'un réacteur nucléaire tombe en panne, le réseau perd plus de 1 000 MW quasi-instantanément ; ces caractéristiques les rendent indispensables à la sécurité du système électrique.
-
--Elle représente 12 à 14 % de la production totale d’électricité
-
-- 1/3 de l’énergie électrique renouvelable française.
-
--La France est le 2e pays européen producteur d’hydroélectricité, derrière la Norvège.
-
-- La production d'électricité annuelle d'une centrale hydroélectrique d'une puissance de 1MW permet d'alimenter environ 1 700 habitants toute l'année.
-
-Les avantages et les inconvénients dépendent du type de centrale hydroélectrique :
-
-Les centrales aux fils de l’eau avec une hauteur de chute basse (inférieur à 50m) sont moins onéreuse mais produisent moins comparé à des centrales hydroélectrique accolé à un barrage. Elle sont aussi beaucoup moins dangereuse sur le plan environnemental que des centrales à hautes chutes (200m ou plus)  car les barrages à basse chutes doivent moins retenir d’eau et donc moins de pression, ce qui signifie qu’en cas de rupture elles inondent beaucoup moins.
-
-Un avantage des centrales hydroélectrique est le stockage d’énergie, en pompant de l’eau du bas jusqu’en haut du barrage
+Les barrages hydroéléctriques ont été un des premiers moyens de production éléctrique,
+leur production représente aujourd'hui environ 10% du mix éléctrique,
+sa part est en augmentation, particulièrement depuis la fin de la seconde 
+guerre mondiale.
+<br/><br/>
+Ils ont un rendement d'environ 80% et représente 51% de la production éléctrique 
+issue d'énérgies renouvelables.
+<br/><br/>
+La production d'électricité annuelle d'une centrale hydroélectrique d'une 
+puissance de 1MW permet d'alimenter environ 1 700 habitants toute l'année.
+<br/>
+Une caractéristique intérréssante des centrales hydroélectrique est le stockage d’énergie, en pompant de l’eau du bas jusqu’en haut du barrage
 
 				</v-card-text>
 			</v-card>
 			<v-card class='mt-2 px-3'>
 				<v-card-title class='title'>Comment ça marche?</v-card-title>
 				<v-card-text>
+- permettent de concentrer leur production pendant les périodes de forte consommation. elles sont capables de passer de l'arrêt
+ à leur puissance maximale en quelques minutes, et donc de faire face aux variations parfois très rapides de la demande ou de l'offre 
+ par exemple, lorsqu'un réacteur nucléaire tombe en panne, le réseau perd plus de 1 000 MW quasi-instantanément ; ces caractéristiques 
+ les rendent indispensables à la sécurité du système électrique.
+
+
+
+Les centrales aux fils de l’eau avec une hauteur de chute basse (inférieur à 50m) 
+sont moins onéreuse mais produisent moins comparé à des centrales hydroélectrique accolé à un barrage. 
+Elle sont aussi beaucoup moins dangereuse sur le plan environnemental que des centrales à hautes chutes (200m ou plus)  
+car les barrages à basse chutes doivent moins retenir d’eau et donc moins de pression, ce qui signifie qu’en cas de rupture 
+elles inondent beaucoup moins.
 				</v-card-text>
 			</v-card>
 				<v-layout mt-5 color='white--text'>
@@ -70,6 +61,7 @@ Un avantage des centrales hydroélectrique est le stockage d’énergie, en pomp
 								<ul>
 									<li>Risques et problèmes, catastrophes écologiques etc. en cas de rupture d’un barrage</li>
 									<li>Besoin de déplacer des populations et de noyer des terres agricoles lors de la construction</li>
+									<li>Offrent un "moyen de stockage" éléctrique à grande echelle et sur le long terme</li>
 								</ul>
 							</v-card-text>
 						</v-card>
