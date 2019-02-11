4601a10ed7f04b325460f30918c057654873ca5a
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-02-11 15:30:58 +0100

    début fesage nucléaire

--- src/views/Pourquoi/moyensProd/Nucleaire.vue
+++ src/views/Pourquoi/moyensProd/Nucleaire.vue
@@ -1,58 +1,68 @@
 <template>
-	<v-card class='pa-4'>
-		<v-card-title class='display-1'>Les centrales nucléaires</v-card-title>
-		<v-card class='mt-2 px-3' flat>
-
-Nucléaire
-Cela représente environ 75% de la production d’énergie électrique en France. On l’a dit, ce savoir faire français en matière de nucléaire depuis les années 80 permet à la France de se placer deuxième mondial derrière les états unis avec ses dix-neuf centrales nucléaires. L’électricité est produite en récupérant l'énergie issue de la fission d’atomes d’uranium (schéma interactif de la fission nucléaire). Ce moyen subit un déclin de son utilisation depuis les années 2010.
-
-Rendement: ≃33%
-Part:≃71% 
-
-
-Constat: Malgré le fait que la filière nucléaire essuie un déclin économique notable ainsi
- qu’un coût de plus en plus important, on vois que la France est toujours aussi attaché à sa
-  filière nucléaire avec plus de 70% de part dans le mix énergétique électrique en 2017. D’un
-   point de vue économique, le nucléaire représente un fardeau même un gouffre économique pour
-l’Etat, dont on ne peut se détacher (emplois, leaders industriels français): le nucléaire étant très lié à l’état
-
-
-
-Nucléaire
-Environ 90% de la production française est convertie en électricité
-des pertes de rendement importantes.
-On estime qu’une centrale thermique consomme 2 fois plus d’énergie primaire qu’elle n’en restitue sous forme d’électricité (un rendement de 33% est retenu comme convention).
-Coûts d’installation, d’entretien et de démantèlement (lorsque les centrales sont trop vieilles) > Coût très élevé
-Conséquences en cas d’accident (radioactivité)
-Sortie du nucléaire en Allemagne en 2011
-Sortie du nucléaire en Italie en 2011
-Impasse dans les pays occidentaux de la filière nucléaire
-Accident de la centrale de Fukushima au Japon
-
-
-
-
-
-			<v-layout>
-				<v-flex xs6>
-		<v-card-title class='title'>Avantages:</v-card-title>
-		
-grande production énergétique en France
-pas de rejet de CO2
-				</v-flex>
-				<v-flex xs6>
-		<v-card-title class='title'>Désavantages:</v-card-title>
-		
-potentiellement très dangereux et irréversible (centrale de Fukushima au Japon) 
-grande pollution planétaire (radioactivité)
-				</v-flex>
-			</v-layout>
+		<v-card class='pa-4'>
+			<v-card-title class='display-2'>Énergie nucléaire</v-card-title>
+			<v-card flat class='my-4 px-5 title'>
+				En France, la prodution d'éléctricité nucléaire est mise en place dans les années 1950 et 1960.
+			</v-card>
+			<v-card class='mt-2 px-3'>
+				<v-card-text>
+Elle est ensuite devenue progressivement la principale source de production d'électricité en France. Le nucléaire couvre, en 2016, 
+72 % de la production d'électricité.
+<v-layout py-5>
+	<v-flex xs6>
+		<v-img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Produc-energie-primaire-fr.svg/330px-Produc-energie-primaire-fr.svg.png' aspect-ratio="1.7" contain></v-img>
+	</v-flex>
+	<v-flex xs6>
+		1tep correspond à l'énergie dégagée sous forme de chaleur par la combustion d’une tonne de pétrole.
+		<br/><br/>
+		L'énergie produite par 1 kg d'uranium naturel dans un réacteur nucléaire est égale à l'énergie produite par 
+		la combustion de 10 tonnes de pétrole.
+	</v-flex>
+</v-layout>
+La France a bien heureusement décidé de réduire la part du nucléaire dans sa production d’électricité de moitié avant 2025
+lors de l'adoption de la loi relative à la transition énergétique pour la croissance verte de 20151. 
+<br/><br/>
+En 2017, la France compte 19 centrales nucléaires en exploitation pour un total de 58 réacteurs nucléaires et se place donc deuxième 
+au rang mondial derrière les états unis.
+				</v-card-text>
+			</v-card>
+			<v-card class='mt-2 px-3'>
+				<v-card-title class='title'>Comment ça marche?</v-card-title>
+				<v-card-text>
+				</v-card-text>
+			</v-card>
+				<v-layout mt-5 color='white--text'>
+					<v-flex xs1>
+					</v-flex>
+					<v-flex xs5>
+						<v-card  style='height: 100%;' color='light-green lighten-1 white--text'>
+							<v-card-title class='title'>Avantages:</v-card-title>
+							<v-card-text>
+								<ul>
+									<li>pas de rejet de CO2</li>
+								</ul>
+							</v-card-text>
+						</v-card>
+					</v-flex>
+					<v-flex xs1>
+					</v-flex>
+					<v-flex xs5>
+						<v-card  style='height: 100%;' color='red lighten-1 white--text'>
+							<v-card-title class='title'>Désavantages:</v-card-title>
+							<v-card-text>
+								<ul>
+									<li>Conséquences en cas d’accident</li>
+									<li>Difficultés à traiter les déchets</li>
+									<li>Coûts d’installation, d’entretien et de démantèlement élevées</li>
+								</ul>
+							</v-card-text>
+						</v-card>
+					</v-flex>
+					<v-flex xs1>
+					</v-flex>
+				</v-layout>
 		</v-card>
-	</v-card>
 </template>
-
-<script>
-export default {
-
-}
-</script>
+<style scoped>
+* {font-size: 20px;}
+</style>
--- src/views/Pourquoi/moyensProd/Solaire.vue
+++ src/views/Pourquoi/moyensProd/Solaire.vue
@@ -11,13 +11,15 @@
 					 Il s’agit de la Nouvelle Aquitaine, l’Occitanie, et la Provence-Alpes-Côte-d’Azur.
 					 <v-img src='https://www.edf.fr/sites/default/files/mediatheque/2018/solaire/energie-a-z_solaire-pv_cap-regions_2018.png' aspect-ratio="1.7" contain></v-img>
 					Cela montre à quelle point la production issue de l'énérgie solaire est dépendante de la position géographique et de la météo.
-				</v-card-text>
-			</v-card>
-			<v-card class='mt-2 px-3'>
-				<v-card-text>
+					<br/><br/>
 					La consommation d’électricité d’origine photovoltaïque représente 2 % de la consommation d’électricité nationale en 2017. 
 					Par comparaison, elle avoisine les 7 % en Allemagne, en Italie et en Grèce. <br/><br/>
 				Les panneaux photovoltaïques ont un trés faible rendement: seulement 5% de l'énérgie solaire est transformée en éléctricité.
+				</v-card-text>
+			</v-card>
+			<v-card class='mt-2 px-3'>
+				<v-card-title class='title'>Comment ça marche?</v-card-title>
+				<v-card-text>
 				</v-card-text>
 			</v-card>
 				<v-layout mt-5 color='white--text'>
