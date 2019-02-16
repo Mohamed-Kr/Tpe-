26f378ee9ad3f6501880bcfa2f7182252b6d0296
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-02-16 11:52:08 +0100

    parti conso

--- src/views/Comment/1.vue
+++ src/views/Comment/1.vue
@@ -3,40 +3,54 @@
   <v-layout justify-center align-center fill-height>
     <v-flex xs11 md9 lg10 xl8>
       <v-card class='subheading mx-3 px-3 transparent' flat>
-        <v-card-title class='headline white--text'>Si on réfléchit bien: sans consomation, pas besoin de production.
-        </v-card-title>
+        <v-card-title class='headline white--text'>La consomation est elle aussi à remettre en question.</v-card-title>
       </v-card>
       <v-card class='subheading mx-5 mt-4 pa-3'>
         <v-card-text>
           <v-card-title class='title'>Une consommation inconstante</v-card-title>
-          <v-card class='mt-2 px-3' flat
-          >La consommation est sensible à la météo et plus particulièrement à la température. Par exemple en hiver, plus il fait froid plus les ménages et les entreprises consomment de l'électricité pour se chauffer. On dit que la consommation est thermosensible.
-De même, la consommation est beaucoup moins élevée la nuit car les industries ne fonctionnent pas et les ménages consomment moins.
-          </v-card>
+            <v-card class='mt-2 px-3' flat
+              >La consommation est sensible à la météo et plus particulièrement à la température. Par exemple en hiver, 
+              plus il fait froid plus les ménages et les entreprises consomment de l'électricité pour se chauffer. 
+              On dit que la consommation est thermosensible. De même, la consommation est beaucoup moins élevée la nuit 
+              car les industries ne fonctionnent pas et les ménages consomment moins.
+              >Cette inconstance est assez problématique au niveau de la production car sur d’aussi grosses échelles, 
+              les moyens de stockage électrique actuels sont encore trop peu performants, c'est donc à la production 
+              de s'adapter.
+              <br/>
+              <br/>La consommation en France reste tout de même assez stable, entre 450 TWh et 500 TWh, pourtant la consommation 
+              des ménages est en constante augmentation, mais cette augmentation est compensé par la baisse de consommation 
+              au niveau des industries due à la mondialisation.
+            </v-card>
         </v-card-text>
       </v-card>
-      <v-card class='subheading ma-5 pa-3'>
+      <v-card class='subheading mx-5 mt-4 pa-3'>
         <v-card-text>
-          <v-card-title class='title'>Une inconstance problématique</v-card-title>
-          <v-card class='mt-2 px-3' flat
-          >Cette inconstance est assez problématique au niveau de la production car sur d’aussi grosses échelles, les moyens de stockage électrique actuels sont encore trop peu performants,
-Les moyens de production électrique s’adaptent donc, par exemple, les pales ré-orientables des barrages permettent de concentrer leur production pendant les périodes de forte consommation. Elles sont capables de passer de l'arrêt à leur puissance maximale en quelques minutes (voir partie sur l'hydroélectricité).
-La consommation est pourtant le facteur le plus important car plus nous consommons, plus nous devons produire, nous avons donc le devoir de veiller sur notre consommation.
-          
-		  
-		  
--la consommation en france reste assez stable, entre 450 TWh et 500 TWh, pourtant la consommation des ménages est en constante augmentation, mais cette augmentation est compensé par la baisse de consommation au niveau des industries due à la mondialisation.
-
-
-
-II.C.1 Comment réduire la conso°
-thermosensibilité: norme RT2012 : en conséquence il faut réduire la part du chauffage électrique dans les nouveau bâtiments
-éclairage intelligent
-ampoules halogènes par des ampoules plus performante (LED, fluorescente)
-l'ADEME: le terme “gisements d'économie d'énergie”
-l'éclairage nocturne inutile
-publicité
-		  </v-card>
+          <v-card-title class='title'>Une consommation grandissante</v-card-title>
+            <v-card class='mt-2 px-3' flat
+              >Malgrés la part durable de la production éléctrique encore assez petite, la consommation ne fait que grandir.
+              Il y a eu le chauffage éléctrique qui est devenu tellement commun que chaque degré celcius en moins dans la température moyenne
+              du pays fait augmenter la consommation de 2400 MW...
+              <br/>
+              <br/>Nous pensons que la démocratisation de l'utilisation de l'éléctricité est en grande partie 
+              dûe au fait que nous avons tendance à ne pas considérer la production de l'éléctricité que 
+              l'on consomme mais seulement la manière dont on la consomme. Par exemple, nous aurions tendance
+              à penser qu'une voiture éléctrique polluerait beaucoup moins qu'une voiture fonctionnant à l'essence,
+              ce qui est vrai si nous ne comparons que la façon dont l'énérgie est consoméé mais si nous prenons en compte 
+              la production des voiture et les moyens d'obtenir l'énérgie, pour une même utilisation quasi-quotidienne
+              la voiture éléctrique ne devient plus 'rentable écologiquement' qu'aprés quatre à cinq ans.
+            </v-card>
+        </v-card-text>
+      </v-card>
+      <v-card class='subheading mx-5 mt-4 pa-3'>
+        <v-card-text>
+          <v-card-title class='title'>Un nouveau rapport à la consommation</v-card-title>
+            <v-card class='mt-2 px-3' flat
+              >La consomation éléctrique actuelle (et plus particulièrement son évolution) referme un peu la perspective d'un 
+              rapport à l'éléctricité durable. Mais bienheureusement, il y a de plus en plus de geste visant à diminuer la consommation,
+              l'état à par exemple mis en place des moyens d'isoler sa maison, sous les frais des entreprise comme Total, ou 
+              encore des entreprises rémunéré pour l'éléctricité dont ils ont éviter la consommation inutile, 
+              par exemple à l'aide de chauffages inteligents.
+            </v-card>
         </v-card-text>
       </v-card>
     </v-flex>
@@ -44,11 +58,6 @@ publicité
 </v-container>
 </template>
 
-<script>
-export default {
-  components: {}
-}
-</script>
 
 
 
