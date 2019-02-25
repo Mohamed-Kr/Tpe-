0e82f9253a935be40514e0c876823600d56ef578
Mohamed-Kr <43796250+Mohamed-Kr@users.noreply.github.com>
2019-02-25 19:47:21 +0100

    update mdr

--- src/views/Conclusion/1.vue
+++ src/views/Conclusion/1.vue
@@ -1,6 +1,18 @@
 <template>
 	<v-container>
-		nhgfngfn
+		<template>
+			<v-container justify-center align-center mt-2>
+				<v-layout justify-center>
+				<v-flex xs11 md12 lg10 xl8 white--text mb-5>
+					<v-card>
+					<v-card-text style="font-size: 1.5rem">
+						EN gros ba voila quoi, mé nou vin mintenant stp.
+					</v-card-text>
+					</v-card>
+				</v-flex>
+				</v-layout>
+			</v-container>
+		</template>
 	</v-container>
 </template>
 
--- src/views/Conclusion/2.vue
+++ src/views/Conclusion/2.vue
@@ -1,15 +1,23 @@
 <template>
 	<v-container>
-		tgr
+		<v-list>
+			<v-list-tile v-for="source in sources" :key='source'>
+				<v-list-tile-action>
+					<a :href='source.link'>{{ source.name }}</a>
+				</v-list-tile-action>
+			</v-list-tile>
+		</v-list>
 	</v-container>
 </template>
 
 <script>
 export default {
-
+	data: () => ({
+	sources: [
+			{link: 'https://youtube.com', name: 'Youtube'},
+			{link: 'https://youube.com', name: 'Youube'}
+		]
+	})		
 }
 </script>
 
-<style>
-
-</style>
