99654c5d675e698f9ddbe5d36e19934beb8762f4
Le_Futuriste <spamfree@matthieubessat.fr>
2019-02-26 16:30:35 +0100

    conclusion

--- src/views/Conclusion/1.vue
+++ src/views/Conclusion/1.vue
@@ -1,25 +1,43 @@
 <template>
-	<v-container>
-		<template>
-			<v-container justify-center align-center mt-2>
-				<v-layout justify-center>
-				<v-flex xs11 md12 lg10 xl8 white--text mb-5>
-					<v-card>
-					<v-card-text style="font-size: 1.5rem">
-						EN gros ba voila quoi, mé nou vin mintenant stp.
-					</v-card-text>
-					</v-card>
-				</v-flex>
-				</v-layout>
-			</v-container>
-		</template>
-	</v-container>
+  <v-container>
+    <template>
+      <v-container justify-center align-center mt-2>
+        <v-layout justify-center>
+          <v-flex xs11 md12 lg10 xl8 white--text mb-5>
+            <v-card>
+              <v-card-text style="font-size: 1.5rem">
+                <p>
+                  On voit donc que notre mix électrique est encore en grande partie composé d'énergies non
+                  renouvelables et n'est par conséquent pas viable sur le long terme.
+                </p>
+                <p>
+                  Même si un mix 100% renouvelable est techniquement possible à mettre en place,
+                  il n'existe pas de solution miracle, il faudra forcément faire des efforts aussi bien
+                  du côté de la production que de la consommation pour y parvenir.
+                </p>
+                <p>
+                  La solution consiste dans un premier temps à déveloper les points production renouvelable, passer
+                  à un réseau beaucoup plus décentralisé, plus intelligent avec une multitude de petites unités de productions.
+                  Aussi, la problématique du renouvelable est totalement lié à la consommation, nous devrons donc
+                  piloter la consomation à grande échelle pour éviter les pics et ainsi développer un réseau intelligent.
+                </p>
+                <p>
+                  Enfin, nos traveaux se concentrent sur l'objectif final et non sur la transition en elle même,
+                   les aspects sociaux et économique sont aussi à prendre en compte.
+                  Notre rapport à l'électricité est donc à remettre profondément en question mais nous restons tout de
+                  même optimiste.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </template>
+  </v-container>
 </template>
 
 <script>
-export default {
-
-}
+  export default {}
 </script>
 
 <style>
